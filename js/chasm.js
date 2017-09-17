d86e805a4f2f29f78fa01d2b3ba9be818c309ffe
Ting Lew <ting.lew@gmail.com>
2017-09-17 12:15:01 -0400

    add chasm

--- js/chasm.js
+++ js/chasm.js
@@ -1,18 +1,21 @@
-let x = -80;
-let y = -24.5;
-let personX = -5;
-let personY = -45;
+let x = -45;
+let y = -40.59;
+let personX = 30;
+let personY = -61.1;
+let steps = 0;
+yearCount = 11;
 
 let transformIncrementX = 5;
 let transformIncrementY = 2.3;
 $("body").keydown(function(e) {
 
-  if (personX > -20) {
-    if(e.keyCode == 39) { // right
+  if (steps < yearCount) {
+    if(e.keyCode == 39) { // right, forward in time, wider
       x-=transformIncrementX;
       y+=transformIncrementY;
-      personX -=transformIncrementX;
-      personY +=transformIncrementY;
+      personX -= transformIncrementX;
+      personY += transformIncrementY;
+      steps++;
 
       let transformString = "translateX(" + x.toString() + "vh) translateY(" + y.toString() + "vh) rotate(-25deg) skewX(40deg)";
       $("#chasm-bottom").css("transform", transformString);
@@ -22,19 +25,22 @@ $("body").keydown(function(e) {
     }
   }
 
-  if (personX < 30) { // leave a small space bn the chasms
-    if(e.keyCode == 37) { // left
-    x+=transformIncrementX;
-    y-=transformIncrementY;
-    personX += transformIncrementX;
-    personY -= transformIncrementY;
-    let transformString = "translateX(" + x.toString() + "vh) translateY(" + y.toString() + "vh) rotate(-25deg) skewX(40deg)";
+  if (steps > 0) {
 
-    let personTransform = "translateX(" + personX.toString() + "vh) translateY(" + personY.toString() + "vh)";
-    $("#person").css("transform", personTransform);
+    if(e.keyCode == 37) { // left, go backwards in time, narrow
+      x+=transformIncrementX;
+      y-=transformIncrementY;
+      personX += transformIncrementX;
+      personY -= transformIncrementY;
+      steps--;
 
-    $("#chasm-bottom").css("transform", transformString);
-  }
+      let transformString = "translateX(" + x.toString() + "vh) translateY(" + y.toString() + "vh) rotate(-25deg) skewX(40deg)";
+
+      let personTransform = "translateX(" + personX.toString() + "vh) translateY(" + personY.toString() + "vh)";
+      $("#person").css("transform", personTransform);
+
+      $("#chasm-bottom").css("transform", transformString);
+    }
   }
 
 });
